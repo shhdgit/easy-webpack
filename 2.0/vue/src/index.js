9a8f25eda41cdcc5844f305686087a18c527cc42
suhaha <jklopsdfw@gmail.com>
2017-01-16 11:45:29 +0800

    Structure Adjustment

--- 2.0/vue/src/index.js
+++ 2.0/vue/src/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import {sync} from 'vuex-router-sync'
-import router from 'config/router'
+import router from 'config/routes'
 import store from 'store'
 import App from 'components/App'
 
