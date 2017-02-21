28aa4cc1fd16ab0ef97b5a45ce59dcfe752108f7
suhaha <jklopsdfw@gmail.com>
2017-02-21 13:11:28 +0800

    Use Yarn

--- 2.0/vue/build/init.sh
+++ 2.0/vue/build/init.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-npm i -D webpack \
-         webpack-merge \
-         webpack-dev-server \
-         html-webpack-plugin \
-         extract-text-webpack-plugin@beta \
-         babel-core \
-         babel-loader \
-         babel-preset-es2015 \
-         babel-preset-stage-2 \
-         babel-runtime \
-         cross-env \
-         vue-template-compiler \
-         vue-loader \
-         vue-hot-reload-api \
-         vue-html-loader \
-         vue-style-loader \
-         css-loader \
-         style-loader \
-         file-loader \
-         url-loader \
-         raw-loader \
-         less \
-         less-loader && \
-npm i -S vue \
+yarn add -D webpack \
+            webpack-merge \
+            webpack-dev-server \
+            html-webpack-plugin \
+            extract-text-webpack-plugin@beta \
+            babel-core \
+            babel-loader \
+            babel-preset-es2015 \
+            babel-preset-stage-2 \
+            babel-runtime \
+            cross-env \
+            vue-template-compiler \
+            vue-loader \
+            vue-hot-reload-api \
+            vue-html-loader \
+            vue-style-loader \
+            css-loader \
+            style-loader \
+            file-loader \
+            url-loader \
+            raw-loader \
+            less \
+            less-loader && \
+yarn add vue \
          vue-router \
          axios \
          vuex \
